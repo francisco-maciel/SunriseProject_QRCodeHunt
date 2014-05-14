637d4d91ddba258635da865dcfb8a1e27c210b3e
francisco_maciel <francisco.ma.maciel@gmail.com>
2014-05-14 21:36:42 +0100

    added one equal qr code per 5 seconds

--- qr_code_sunrise/qr.php
+++ qr_code_sunrise/qr.php
@@ -4,8 +4,19 @@ $filename  = dirname(__FILE__).'/data.txt';
 
 if ( isset($_GET['id']))
 {
-    // TODO verificar conteudo do ficheiro igual
-    file_put_contents($filename,$_GET['id']);
+
+    $filetime = filemtime($filename);
+
+   if (strcmp($_GET['id'], file_get_contents($filename)) == 0) {
+       $modify;
+       if(time() - $filetime > 5) {
+           $modify = true;
+       }
+       else $modify = false;
+
+   }
+if ($modify) file_put_contents($filename,$_GET['id']);
+
     echo "TODO: RESPOSTA SUCESSO";
     die();
 
